8479184b0bf9f27a9f7d712285a73cce6c097fab
Hayashi <36870134+macchanameko@users.noreply.github.com>
2018-07-19 13:39:45 +0900

    processing

--- processing/time/build-tmp/source/time.java
+++ processing/time/build-tmp/source/time.java
@@ -17,7 +17,6 @@ public class time extends PApplet {
 public void setup(){
 	
 	frameRate(30);
-	rotate(PI/4);
 }
 
 public void draw(){
@@ -27,22 +26,20 @@ public void draw(){
 	float m = minute();
 	float h = hour()%12;
 
-	int r = 200;
-
 	translate(width/2, height/2);
 	noFill();
 
 	float radius = (width - 50) / 2; 
-	float radius2 = (width - 100) / 2;
-	float radius3 = (width - 200) / 2;
+	float radius2 = (width - 150) / 2;
+	float radius3 = (width - 250) / 2;
   	for(int t = 0; t < 360; t += 6){
 	    stroke(255,100);
 		strokeWeight(10);
 	    
 	    float radianT = radians(t);
-	    float radianS = radians((int)s* 6.0f);
-	    float radianM = radians((int)m* 6.0f);
-	    float radianH = radians((int)h * 30.0f / (3*PI/2));
+	    float radianS = radians((int)s* 6.0f - 90);
+	    float radianM = radians((int)m* 6.0f - 90);
+	    float radianH = radians((int)h * 30.0f - 90);
 
 	    float nowSX = radius3 * cos(radianS);
 	    float nowSY = radius3 * sin(radianS);
@@ -53,7 +50,7 @@ public void draw(){
 	    float nowHX = radius * cos(radianH);
 	    float nowHY = radius * sin(radianH);
 	    
-	    float x =	 radius * cos(radianT);
+	    float x = radius * cos(radianT);
 	    float y = radius * sin(radianT);
 	    
 	    float xx = radius2 * cos(radianT);
@@ -62,22 +59,25 @@ public void draw(){
 	    float xxx = radius3 * cos(radianT);
 	    float yyy = radius3 * sin(radianT);
 
-	   	stroke(0,255,255);
+	   	stroke(255,0,0);
 	   	point(nowSX,nowSY);
+	   	stroke(0,255,0);
 	   	point(nowMX,nowMY);
 	   	stroke(255,100);
 	   	point(xxx,yyy);
 	    point(xx,yy);
 	    if(t % 30 == 0){
 	    	strokeWeight(15);
-	    	stroke(0,255,255);
+	    	stroke(0,0,255);
 	    	point(nowHX,nowHY);
 	    	stroke(255,100);
 	    	point(x,y);
 	    }
 	}
+	textSize(50);
+	text(nf((int)hour(),2)+" : "+nf((int)minute(),2)+" . "+nf((int)second(),2),-150,10);
 }
-  public void settings() { 	size(500,500); }
+  public void settings() { 	size(900,900); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "time" };
     if (passedArgs != null) {
--- processing/time/time.pde
+++ processing/time/time.pde
@@ -1,5 +1,5 @@
 void setup(){
-	size(500,500);
+	size(900,900);
 	frameRate(30);
 }
 
@@ -10,22 +10,20 @@ void draw(){
 	float m = minute();
 	float h = hour()%12;
 
-	int r = 200;
-
 	translate(width/2, height/2);
 	noFill();
 
 	float radius = (width - 50) / 2; 
-	float radius2 = (width - 100) / 2;
-	float radius3 = (width - 200) / 2;
+	float radius2 = (width - 150) / 2;
+	float radius3 = (width - 250) / 2;
   	for(int t = 0; t < 360; t += 6){
 	    stroke(255,100);
 		strokeWeight(10);
 	    
 	    float radianT = radians(t);
-	    float radianS = radians((int)s* 6.0);
-	    float radianM = radians((int)m* 6.0);
-	    float radianH = radians((int)h * 30.0 / (3*PI/2));
+	    float radianS = radians((int)s* 6.0 - 90);
+	    float radianM = radians((int)m* 6.0 - 90);
+	    float radianH = radians((int)h * 30.0 - 90);
 
 	    float nowSX = radius3 * cos(radianS);
 	    float nowSY = radius3 * sin(radianS);
@@ -36,7 +34,7 @@ void draw(){
 	    float nowHX = radius * cos(radianH);
 	    float nowHY = radius * sin(radianH);
 	    
-	    float x =	 radius * cos(radianT);
+	    float x = radius * cos(radianT);
 	    float y = radius * sin(radianT);
 	    
 	    float xx = radius2 * cos(radianT);
@@ -45,18 +43,21 @@ void draw(){
 	    float xxx = radius3 * cos(radianT);
 	    float yyy = radius3 * sin(radianT);
 
-	   	stroke(0,255,255);
+	   	stroke(255,0,0);
 	   	point(nowSX,nowSY);
+	   	stroke(0,255,0);
 	   	point(nowMX,nowMY);
 	   	stroke(255,100);
 	   	point(xxx,yyy);
 	    point(xx,yy);
 	    if(t % 30 == 0){
 	    	strokeWeight(15);
-	    	stroke(0,255,255);
+	    	stroke(0,0,255);
 	    	point(nowHX,nowHY);
 	    	stroke(255,100);
 	    	point(x,y);
 	    }
 	}
+	textSize(50);
+	text(nf((int)hour(),2)+" : "+nf((int)minute(),2)+" . "+nf((int)second(),2),-150,10);
 }
