1c40d897f1800b0969249d1f1096236d9b0c93d0
macchanameko <36870134+macchanameko@users.noreply.github.com>
2018-07-27 12:19:40 +0900

    processing

--- processing/clock/build-tmp/source/clock.java
+++ processing/clock/build-tmp/source/clock.java
@@ -74,7 +74,7 @@ public void draw(){
 	pushMatrix();
 	rotate(radians(s*(360/60)));
 	rect(-r/2+random(-3,3),-r/2+random(-3,3),r,r);
-	line(-r/2,-r/2,-150,-150);
+	line(-r/2+random(-3,3),-r/2+random(-3,3),-150+random(-3,3),-150+random(-3,3));
 	popMatrix();
 
 	strokeWeight(4);
@@ -83,7 +83,7 @@ public void draw(){
 	rotate(radians(m*(360/60)));
 	rect(-r/2+random(-3,3),-r/2+random(-3,3),r,r);
 	stroke(0,0,255,25);
-	line(-r/2,-r/2,-50,-50);
+	line(-r/2+random(-3,3),-r/2+random(-3,3),-50+random(-3,3),-50+random(-3,3));
 	popMatrix();
 
 	strokeWeight(5);
@@ -92,7 +92,7 @@ public void draw(){
 	rotate(radians(h*(360/12)));
 	rect(-r/2+random(-3,3),-r/2+random(-3,3),r,r);
 	stroke(0,255,255,25);
-	line(-r/2,-r/2,-100,-100);
+	line(-r/2+random(-3,3),-r/2+random(-3,3),-100+random(-3,3),-100+random(-3,3));
 	popMatrix();
 }
   public void settings() { 	size(displayWidth,displayHeight); 	smooth(); }
--- processing/test/test.pde
+++ processing/test/test.pde
@@ -1,41 +1,17 @@
-import processing.opengl.*;
-
-int r = height*2;
-
+float time = 0;
 void setup(){
-  size(displayWidth,displayHeight,OPENGL);
+  size(displayWidth,displayHeight);
   background(0);
   stroke(255);
+  strokeWeight(5);
 }
 
 void draw(){
   background(0);
-
-  translate(width/2,height/2);
-  rotateY(frameCount * 0.03);
-  rotateX(frameCount * 0.04);
-
-  float s = 0;
-  float t = 0;
-  float lx = 0;
-  float ly = 0;
-  float lz = 0;
-
-  while(t < 180){
-    s += 18;
-    t += 1;
-    float rs = radians(s);
-    float rt = radians(t);
-
-    float tx = 0 + (r * cos(rs) * sin(rt));
-    float ty = 0 + (r * sin(rs) * sin(rt));
-    float tz = 0 + (r * cos(rt));
-
-    if(lx != 0){
-      line(tx,ty,tz,lx,ly,lz);
-    }
-    lx = tx;
-    ly = ty;
-    lz = tz;
+  float x = 0;
+  while(x < width){
+    point(x,height*noise(x/100,time));
+    x += 1;
   }
+  time += 0.02;
 }
